fe7309342e6a29de788dd49be33b80790948a5be
henry <afr_henrylsy@163.com>
2023-11-21 06:37:43 -0800

    add camera_detect

---
+++ record_files/record.sh
@@ -0,0 +1,23 @@
+source ./devel/setup.zsh & sleep 1;
+rosbag record --tcpnodelay \
+  /position_cmd \
+  /ekf/ekf_odom_lidar \
+
+
+# /camera/color/camera_info \
+# /camera/color/image_raw \
+# /camera/depth/image_rect_raw \
+# /camera/depth/camera_info \
+# /camera/infra1/camera_info \
+# /camera/infra1/image_rect_raw \
+# /camera/infra2/camera_info \
+# /camera/infra2/image_rect_raw \
+# /camera/extrinsics/depth_to_color \
+# /camera/extrinsics/depth_to_infra1 \
+# /camera/extrinsics/depth_to_infra2 \
+# /camera/realsense2_camera_manager/bond \
+# /livox/imu \
+# /livox/lidar \
+# /mavros/imu/data \
+# /mavros/imu/data_raw \
+
