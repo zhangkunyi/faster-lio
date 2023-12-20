8147bc2ddc04e448dc5fc93d01a253bbae0cc493
henry <afr_henrylsy@163.com>
2023-12-19 23:50:00 -0800

    remove camera_detect

--- src/camera_detect/aruco_detect.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-if [ "$1" = "true" ]; then
-roslaunch aruco_ros single_test.launch markerId:=582 markerSize:=0.135 & sleep 10
-rosrun image_view image_view image:=/aruco_single/result
-else
-roslaunch aruco_ros single_test.launch markerId:=582 markerSize:=0.135
-fi
